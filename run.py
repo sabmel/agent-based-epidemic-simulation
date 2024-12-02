c0ed528cea0482d4bda62823a7311d2953abeb28
Sabrina Smith <sabmel11@gmail.com>
2024-12-01 22:01:12 -0700

    updated data visualization

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from model.model import EpidemicModel
 import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
 # Set model parameters
 num_agents = 100
@@ -13,10 +15,19 @@ model = EpidemicModel(num_agents, initial_infected, transmission_prob, recovery_
 for i in range(max_steps):
     model.step()
 
-# Retrieve and plot data
+# Retrieve data
 data = model.datacollector.get_model_vars_dataframe()
-data.plot()
+data.reset_index(inplace=True)
+data_melted = data.melt(id_vars=["index"], value_vars=["Susceptible", "Infected", "Recovered"],
+                        var_name="State", value_name="Count")
+
+# Seaborn Plot
+sns.set(style="darkgrid")
+plt.figure(figsize=(12, 6))
+sns.lineplot(data=data_melted, x="index", y="Count", hue="State", marker="o")
 plt.xlabel("Time Step")
 plt.ylabel("Number of Agents")
 plt.title("Epidemic Simulation Over Time")
+plt.legend(title="Agent State")
+plt.tight_layout()
 plt.show()
